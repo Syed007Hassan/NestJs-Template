7cc9866460f8ac0023937d3a8b9e23bce98002eb
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-10-15 03:13:26 +0500

    sample .env added

--- Template-1/src/app.module.ts
+++ Template-1/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PostgreSqlDataSource } from './config/ormConfig';
